0b571d98d307353f0fa8c1384720028b54236e4f
nicholasgonzalez5 <nagonzalez8530@gmail.com>
2023-03-03 07:10:07 -0700

    finsihed mystats

--- myrate/myrate_frontend/src/Components/MyStats.js
+++ myrate/myrate_frontend/src/Components/MyStats.js
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
-import ScrollMenu from 'react-horizontal-scrolling-menu'
 import axios from 'axios';
-import CollectionItems from "./CollectionItems";
-import LoginForm from "../Components/LoginForm";
-import AddCollectionModal from "./Modals/AddCollectionModal";
-//import { param } from "../../../myrate_backend/server/routes/ratings";
 
 const MyStats = () => {
-    const [BookStat, setBookStat] = useState();
-    const [MovieStat, setMovieStat] = useState();
-    const [TVStat, setTVStat] = useState();
+    let [BookStat, setBookStat] = useState(0);
+    let [MovieStat, setMovieStat] = useState(0);
+    let [TVStat, setTVStat] = useState(0);
     const [modalOpen, setModalOpen] = useState(false); 
     const userProfile = useSelector((state) => { return state.userProfile; });
 
@@ -30,9 +25,38 @@ const MyStats = () => {
         })
         .then(function (response) {
                 // set stats with user data with data in the response
-                console.log("response: " + JSON.stringify(response.data));
+                var bs = 0;
+                var ms = 0;
+                var tvs = 0;
+                response.data.map(d => {
+                    if(d["media_type"] === "books") {
+                        bs++;
+                    } else if(d["media_type"] === "movies") {
+                        ms++;
+                    } else {
+                        tvs++;
+                    }
+                });
+
+                setBookStat(bs);
+                setMovieStat(ms);
+                setTVStat(tvs);
             });
     }, [userProfile]);
+
+    return (
+        <><dl>
+            <dt><strong>Books</strong></dt>
+                <dd><li><i>Amount of Books Read: {BookStat}</i></li></dd>
+                <dd><li><i>Time Spent Reading: {BookStat * 600} mins!</i></li></dd>
+            <dt><strong>Movies</strong></dt>
+                <dd><li><i>Amount of Movies Watched: {MovieStat}</i></li></dd>
+                <dd><li><i>Time Spent Watching Movies: {MovieStat * 110} mins!</i></li></dd>
+            <dt><strong>TV Shows</strong></dt>
+                <dd><li><i>Amount of TV Shows Watched: {TVStat}</i></li></dd>
+                <dd><li><i>Time Spent Watching TV Shows: {TVStat * 16 * 40} mins!</i></li></dd>
+        </dl></>
+    )
 }
 
 
