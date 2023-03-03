73606d5e873d5678fa7e2dec905f07eb96033153
Alyse Palsulich <brpalsulich@gmail.com>
2023-03-03 06:12:54 -0700

    Updating secondary search page to include pagination.

--- myrate/myrate_frontend/src/Screens/SearchPage.js
+++ myrate/myrate_frontend/src/Screens/SearchPage.js
@@ -8,6 +8,7 @@ import "./SearchPage.css";
 const SearchPage = () => {
 
   const [searchKey, setSearchKey] = useState("");
+  const [pageNum, setPageNum] = useState(0);
 
   const [bookDetails, setBookDetails] = useState("");
   const [calls, setCalls] = useState("");
@@ -76,7 +77,20 @@ const setBookCard = (key, author, title) => {
             console.log("book type: " + typeof book);
             console.log("cover_i: " + typeof book.cover_i);
             console.log("book in add books: " + book.cover_i)
-            loadBooks.push(book);
+            loadBooks.push(
+              
+              <div class="flip-card" style={{ display: 'inline-block' }}>
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <img src={"https://covers.openlibrary.org/b/ID/" + book.cover_i + "-M.jpg"} alt="Cover Image" style={{ width: 180, height: 272 }}></img>
+                  </div>
+                  <div class="flip-card-back">
+                    <h3>{book.title}</h3>
+                    <p>{book.author_name}</p>
+                  </div>
+                </div>
+              </div>
+            );
           }
         }
 
@@ -84,22 +98,22 @@ const setBookCard = (key, author, title) => {
     if(!loading && res)
     {
       return (
-        <div style={{display : 'inline-block'}}>
+        <div >
           {
-            (loadBooks.map(book => (
-              <div class="flip-card" style={{display : 'inline-block'}}>
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img src={"https://covers.openlibrary.org/b/ID/"+book.cover_i +"-M.jpg"} alt="Cover Image" style={{width:180, height:272}}></img>
-                    </div>
-                  <div class="flip-card-back">
-                    <h3>{book.title}</h3>
-                    <p>{book.author_name}</p>
-                  </div>
-                </div>
-              </div>
-
-            )))
+            <><table>
+              <tr>
+                <th>Results</th>
+              </tr>
+              {DisplayTable(loadBooks, 4, 12)}
+              <tr >
+              </tr>
+            </table>
+              <nav aria-label="...">
+                <ul class="pagination">
+                  {DisplayFooter(loadBooks.length, 4, 12)}
+                </ul>
+              </nav>
+              </>
           }
         </div>
       )
@@ -111,6 +125,96 @@ const setBookCard = (key, author, title) => {
   useEffect(() => {
   }, [loading, error]);
 
+    // mediaList: contains media to display
+  // rowNum: the number of media to display in each row
+  // displayNum: total number of media to display
+  const DisplayTable = (mediaList, rowNum, displayNum) => {
+    let mediaCount = mediaList.length;
+    let currLoc = pageNum * displayNum;
+    let maxlength = currLoc + displayNum;
+    let result = [];
+    // ensure we don't try to access outside of length of media
+    if(maxlength > mediaCount)
+      maxlength = mediaCount;
+    for(let i = currLoc; i < maxlength; i += rowNum)
+    {    
+      let j = i;  
+          result.push(
+            <tr>
+            {
+              <>
+                <td> {mediaList[j]} </td>
+                <td> {mediaList[j+1]} </td>
+                <td> {mediaList[j+2]} </td>
+                <td> {mediaList[j+3]} </td>
+              </>                  
+            }
+          </tr>
+          )
+    }
+    return result;
+  }
+
+  const DisplayFooter = (mediaCount, rowLength, displayLength) =>
+  {
+    // get how many pages there are
+    let numPagesDis = mediaCount/displayLength;
+    let result = [];
+    if (pageNum > 0) {
+      result.push(
+        <li class="page-item">
+          <a class="page-link" href="#" onClick={() => setPageNum(pageNum - 1)}>Previous</a>
+        </li>
+      )
+    }
+    else {
+      result.push(
+        <li class="page-item disabled">
+        <span class="page-link">Previous</span>
+      </li>
+      )
+    }
+    
+    // add current page, page 0, and max page to footer
+    result.push(
+      <li class="page-item ">
+        <span class="page-link" onClick={() => setPageNum(0)}>
+          {1}
+        </span>
+      </li>
+    );
+    result.push(
+      <li class="page-item active">
+        <span class="page-link" >
+          {pageNum+1}
+        </span>
+      </li>
+    );
+    result.push(
+      <li class="page-item ">
+        <span class="page-link" onClick={() => setPageNum(Math.round(numPagesDis-1))}>
+          {Math.round(numPagesDis)}
+        </span>
+      </li>
+    );
+
+    if (pageNum < numPagesDis-1) {
+      result.push(
+        <li class="page-item">
+        <a class="page-link" href="#" onClick={() => setPageNum(pageNum+1)}>Next</a>
+      </li>
+      )
+    }
+    else {
+      result.push(
+        <li class="page-item disabled">
+        <span class="page-link">Next</span>
+      </li>
+      )
+    }
+
+    return result;
+  }
 
   return (
     <>
