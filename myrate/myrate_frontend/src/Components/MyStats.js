a1e28e31cd5c7c4fd96103690a8ad8df6ea9333c
nicholasgonzalez5 <nagonzalez8530@gmail.com>
2023-03-14 03:03:36 -0600

    Basic bar chart and pie chart for mystats

--- myrate/myrate_frontend/src/Components/MyStats.js
+++ myrate/myrate_frontend/src/Components/MyStats.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { ResponsiveBar } from '@nivo/bar';
+import { ResponsivePie } from '@nivo/pie';
 
 const MyStats = () => {
     let [BookStat, setBookStat] = useState(0);
@@ -45,6 +46,89 @@ const MyStats = () => {
             });
     }, [userProfile]);
 
+    const data = [
+      {
+        media: "Book",
+        minutes: BookStat * 600
+      },
+      {
+        media: "Movie",
+        minutes: MovieStat * 110
+      },
+      {
+        media: "TV Show",
+        minutes: TVStat * 16 * 40
+      }
+    ];
+  
+    const Bar = () => {
+      return (
+        <ResponsiveBar
+          data={data}
+          keys={["minutes"]}
+          indexBy="media"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.4}
+          valueScale={{ type: "linear" }}
+          colors="#3182CE"
+          animate={true}
+          enableLabel={false}
+          axisTop={null}
+          axisRight={null}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "minutes",
+            legendPosition: "middle",
+            legendOffset: -40
+          }}
+        />
+      );
+    };
+
+    const pieData = [
+      {
+        id: "Books",
+        label: "Books",
+        value: BookStat,
+        color: "hsl(90, 70%, 50%)"
+      },
+      {
+        id: "Movie",
+        label: "Movie",
+        value: MovieStat,
+        color: "hsl(56, 70%, 50%)"
+      },
+      {
+        id: "TV Show",
+        label: "TV Show",
+        value: TVStat,
+        color: "hsl(103, 70%, 50%)"
+      }
+    ];
+
+    const Pie = () => {
+      return (
+        <ResponsivePie
+          data={pieData}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+        />
+      );
+    };
+
     return (
         <><dl>
             <dt><strong>Books</strong></dt>
@@ -56,66 +140,15 @@ const MyStats = () => {
             <dt><strong>TV Shows</strong></dt>
                 <dd><li><i>Amount of TV Shows Watched: {TVStat}</i></li></dd>
                 <dd><li><i>Time Spent Watching TV Shows: {TVStat * 16 * 40} mins!</i></li></dd>
-        </dl></>
+        </dl>
+        <div style={{height: 600}}>
+          {Bar()}
+        </div>
+        <div style={{height: 600}}>
+          {Pie()}
+        </div></>
     )
 }
 
-
-const data = [
-    {
-      day: "Monday",
-      degress: 59
-    },
-    {
-      day: "Tuesday",
-      degress: 61
-    },
-    {
-      day: "Wednesday",
-      degress: 55
-    },
-    {
-      day: "Thursday",
-      degress: 78
-    },
-    {
-      day: "Friday",
-      degress: 71
-    },
-    {
-      day: "Saturday",
-      degress: 56
-    },
-    {
-      day: "Sunday",
-      degress: 67
-    }
-  ];
-
-  const Bar = () => {
-    return (
-      <ResponsiveBar
-        data={data}
-        keys={["degress"]}
-        indexBy="day"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.4}
-        valueScale={{ type: "linear" }}
-        colors="#3182CE"
-        animate={true}
-        enableLabel={false}
-        axisTop={null}
-        axisRight={null}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "degrees",
-          legendPosition: "middle",
-          legendOffset: -40
-        }}
-      />
-    );
-  };
 
 export default MyStats;
