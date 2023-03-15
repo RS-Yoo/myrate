c78826e8298ce5227651ad273fa5215b1af5aed1
Alyse Palsulich <brpalsulich@gmail.com>
2023-03-15 03:42:25 -0600

    Completed form. TODO: validation of fields

---
+++ myrate/myrate_backend/server/routes/goal.js
@@ -0,0 +1,110 @@
+const express = require("express");
+ 
+// goalRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /goal.
+const goalRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../db/conn");
+ 
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the goals.
+goalRoutes.route("/goal").get(function (req, res) {
+ let db_connect = dbo.getDb("media");
+ db_connect
+   .collection("goals")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+
+// This section will help you get a single goal by id
+goalRoutes.route("/goal/getgoalid/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect
+   .collection("goals")
+   .findOne(myquery, function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+
+
+// This section will help you get a single goal by title and author
+goalRoutes.route("/goal/findgoal").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let title = req.query.goalTitle;
+  let author = req.query.goalAuthor;
+  const query = {goalTitle: title, goalAuthor: author};
+  const goal = db_connect.collection("goals").findOne(query, function (err, result) {
+    if (err) {
+      console.log("error in get goal by title and author: " + err);
+      throw err;
+    }
+    res.json(result);
+  });
+ });
+
+ 
+// This section will help you create a new goal.
+goalRoutes.route("/goal/add").post(function (req, response) {
+ let db_connect = dbo.getDb();
+ let myobj = {
+   username: req.body.username,
+   goalTitle: req.body.goalTitle,
+   mediaType: req.body.mediaType,
+   daymonthyear: req.body.daymonthyear,
+   positive: req.body.positive,
+   amount: req.body.amount,
+   measurement: req.body.measurement,
+ };
+ db_connect.collection("goals").insertOne(myobj, function (err, res) {
+   if (err) throw err;
+   response.json(res);
+ });
+});
+ 
+// This section will help you update a goal by id.
+goalRoutes.route("/update/:id").post(function (req, response) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ let newvalues = {
+   $set: {
+    username: req.body.username,
+    goalTitle: req.body.goalTitle,
+    mediaType: req.body.mediaType,
+    daymonthyear: req.body.daymonthyear,
+    positive: req.body.positive,
+    amount: req.body.amount,
+    measurement: req.body.measurement,
+   },
+ };
+ db_connect
+   .collection("goals")
+   .updateOne(myquery, newvalues, function (err, res) {
+     if (err) throw err;
+     console.log("1 document updated");
+     response.json(res);
+   });
+});
+ 
+// This section will help you delete a goal
+goalRoutes.route("/:id").delete((req, response) => {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect.collection("goals").deleteOne(myquery, function (err, obj) {
+   if (err) throw err;
+   console.log("1 document deleted");
+   response.json(obj);
+ });
+});
+ 
+module.exports = goalRoutes;
--- myrate/myrate_frontend/src/Components/NewGoalForm.js
+++ myrate/myrate_frontend/src/Components/NewGoalForm.js
@@ -2,19 +2,70 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-
+import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import CloseButton from 'react-bootstrap/CloseButton';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Tooltip from "@material-ui/core/Tooltip";
+import Popup from 'reactjs-popup';
 
 function HorizontalExample() {
-    return (
-        <Form>
-            <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">        
-            <Col sm={10}>
-                <Form.Control type="email" placeholder="Goal Name" />
-            </Col>
-            </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+    const userProfile = useSelector((state) => { return state.userProfile; });
+
+    const [goalName, setGoalName] = useState('');
+    const [media_Type, setMediaType] = useState('');
+    const [time_Goal, setTimeGoal] = useState(''); // yearly, monthly, or daily
+    const [t_postitive, setPositive] = useState(''); // true or false
+    const [a_amount, setAmount] = useState('');
+    const [m_measurement, setMeasurement] = useState(''); // minutes, hours, count, etc.
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!isNaN(a_amount))
+        {
+            toast("Amount must be a number.")
+        }
+        if(t_postitive === "on")
+            setPositive(true);
+        else
+            setPositive(false);
+        const data = {
+            username: userProfile.username,
+            goalTitle: goalName,
+            mediaType: media_Type,
+            daymonthyear: time_Goal,
+            positive: t_postitive,
+            amount: a_amount,
+            measurement: m_measurement,
+        };
+        console.log(data);
+        fetch("http://localhost:5000/goal/add", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => {
+                console.log("response from add: " + res);
+            })
+            .catch(error => {
+                console.log(error);
+                //return;
+            });
+       };
+
+    return (
+        <>
+        <Form onSubmit={handleSubmit}>
+ <Form.Group className="mb-3" controlId="goalName" value={goalName} onChange={e => setGoalName(e.target.value)}>
+        <Form.Control as="textarea" rows={1} placeholder="Goal Name" required/>
+      </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword" value={media_Type} onChange={e => setMediaType(e.target.value)}>
                 <Col sm={10}>
                     <Form.Select aria-label="Default select example">
                         <option>Media Type</option>
@@ -25,48 +76,51 @@ function HorizontalExample() {
                 </Col>
             </Form.Group>
             <fieldset>
-                <Form.Group as={Row} className="mb-3">
+                <Form.Group as={Row} className="mb-3" value={time_Goal} onChange={e => setTimeGoal(e.target.value)}>
                     <Col sm={10}>
                         <Form.Check
                             type="radio"
                             label="Yearly"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios1"
+                            value="Yearly"
                         />
                         <Form.Check
                             type="radio"
                             label="Monthly"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios2"
+                            value="Monthly"
                         />
                         <Form.Check
                             type="radio"
                             label="Daily"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios3"
+                            value="Daily"
                         />
                     </Col>
                 </Form.Group>
             </fieldset>
             <Tooltip title="Select if this a goal you WANT to achieve. If this is a goal you want to avoid do not select.">
-                <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+                <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck" value={t_postitive} onChange={e => setPositive(e.target.value)}>
                     <Col sm={10}>
                         <Form.Check label="Positive Goal" />
                     </Col>
                 </Form.Group>
             </Tooltip>
 
-            <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">        
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail" value={a_amount} onChange={e => setAmount(e.target.value)}>        
             <Col sm={10}>
-                <Form.Control type="email" placeholder="Amount" />
+                <Form.Control type="textarea" rows={1} placeholder="Amount" />
             </Col>
             </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword" value={m_measurement} onChange={e => setMeasurement(e.target.value)}>
                 <Col sm={10}>
                     <Form.Select aria-label="Default select example">
                         <option>Measurement</option>
-                        <option value="Books">Media Count</option>
+                        <option value="MCount">Media Count</option>
                         <option value="Minutes">Minutes</option>
                         <option value="Hours">Hours</option>
                     </Form.Select>
@@ -75,10 +129,11 @@ function HorizontalExample() {
 
             <Form.Group as={Row} className="mb-3">
                 <Col sm={{ span: 10, offset: 2 }}>
-                    <Button type="submit">Add</Button>
+                    <Button type="submit" onClick={handleSubmit}>Add</Button>
                 </Col>
             </Form.Group>
         </Form>
+        </>
     );
 }
 
